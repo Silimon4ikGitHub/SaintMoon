aade8b19382cefef58016c78fdd0722c3fdd7d18
Silimon4ikGitHub <115254695+Silimon4ikGitHub@users.noreply.github.com>
2023-04-12 01:39:44 +0200

    Made Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,7 +8,10 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f; // Speed at which the player moves
     private Rigidbody rb; // Reference to the player's Rigidbody component
     [SerializeField] private float offset;
+    [SerializeField] private float rotationSpeed;
     [SerializeField] private GameObject playerHead;
+    [SerializeField] private FixedJoystick joystick;
+
 
     private void Start()
     {
@@ -17,15 +21,52 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Get input from the horizontal and vertical axes
+        MoveByButtoms();
+        MoveByJoystic();
+        RotateByButtons();
+        RotateByJoystick();
+    }
+    private void RotateByButtons()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 upVector = new Vector3(-1, 0, 1);
+        Vector3 rightVector = new Vector3(1, 0, 1);
+
+        playerHead.transform.rotation = Quaternion.LookRotation(upVector * horizontalInput + rightVector * -verticalInput);
+    }
+
+    private void MoveByButtoms()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 upVector = new Vector3 (1, 0, 1);
+        Vector3 upVector = new Vector3(1, 0, 1);
         Vector3 rightVector = new Vector3(-1, 0, 1);
         Vector3 movement = (rightVector * verticalInput + upVector * horizontalInput).normalized * moveSpeed;
 
-        // Apply the movement vector to the player's Rigidbody component
         rb.velocity = movement;
+    }
+
+    private void MoveByJoystic()
+    {
+        float joystickX = joystick.Horizontal;
+        float joystickY = joystick.Vertical;
+
+        Vector3 upVector = new Vector3(-1, 0, 1);
+        Vector3 rightVector = new Vector3(1, 0, 1);
+        Vector3 movement = (rightVector * joystickX + upVector * joystickY).normalized * moveSpeed;
+
+        rb.velocity = movement;
+    }
+
+    private void RotateByJoystick()
+    {
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
+        Vector3 upVector = new Vector3(-1, 0, 1);
+        Vector3 rightVector = new Vector3(1, 0, 1);
+
+        playerHead.transform.rotation = Quaternion.LookRotation(upVector * horizontalInput + rightVector * -verticalInput);
     }
 }
