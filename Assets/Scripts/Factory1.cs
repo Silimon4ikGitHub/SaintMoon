0e804957a319bef1d69404e8700299f952a31f85
Silimon4ikGitHub <115254695+Silimon4ikGitHub@users.noreply.github.com>
2023-04-12 10:46:16 +0200

    Build

--- Assets/Scripts/Factory1.cs
+++ Assets/Scripts/Factory1.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Factory1 : ParentFactory, FactoryInterface
 {
+    public override void MakeResources()
+    {
+        if (GivingStoreSpace[GivingStoreSpace.Length - 1] == null)
+        {
+            if (Resource1 >= Resource1Required)
+            {
+                IsOnTimer = true;
+                if (Timer > MakingTime)
+                {
+                    Resource2++;
+                    Timer = 0;
+                }
 
+            }
+            else IsOnTimer = false;
+        }
+        else IsOnTimer = false;
+    }
 }
